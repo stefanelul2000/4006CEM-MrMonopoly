fd63dc26973ca6a4007c074a18a689adb7cc254c
Stefan Ciubotaru <stefanelul2000@users.noreply.github.com>
2019-10-01 13:12:29 +0100

    Updating discord bot

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-# bot.py
+# Discord Bot
 import os
 import random
 import discord
@@ -18,21 +18,23 @@ async def on_ready():
 
 @client.event
 async def on_member_join(member):
+    greetings=["I've been expecting you"]
     await member.create_dm()
-    await member.dm_channel.send(
-        f'Hi {member.name}, welcome to my Discord Server!'
-    )
+    await member.dm_channel.send(f'{member.name}, welcome to my Discord Server, {greetings}')
 
 
 @client.event
 async def on_message(message):
     id = client.get_guild(484842929865883648)
     channels = ["bot"]
+    greetings=['where have you been',"I've been expecting you",'how can I help you today']
 
     if str(message.channel) in channels:
-        if message.content.find("Hello") != -1:
-            await message.channel.send(f"HI there {message.author.name}!") 
+        if message.content.find("Hello","hello") != -1:
+            await message.channel.send(f"Hi there {message.author.name}, {random.choice(greetings)} !") 
         elif message.content == "!users":
             await message.channel.send(f"""This server has {id.member_count} member(s)!""")
+        elif message.content == "!members":
+            await message.channel.send(f"""This server has {id.member_count} member(s)!""")    
 
 client.run(token)
