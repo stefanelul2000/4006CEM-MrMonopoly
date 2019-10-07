2f10066273764e990f6607e5f1ac2ac44fc5c896
Stefan Ciubotaru <stefanelul2000@users.noreply.github.com>
2019-10-07 21:15:24 +0100

    Update bot.py
    Added role assignation at server join and working hello message

--- bot.py
+++ bot.py
@@ -19,8 +19,16 @@ async def on_ready():
 @client.event
 async def on_member_join(member):
     greetings=["I've been expecting you"]
-    await member.create_dm()
-    await member.dm_channel.send(f'{member.name}, welcome to my Discord Server, {greetings}')
+    role= discord.utils.get(member.guild.roles, name="Users")
+    dm = await member.create_dm()
+
+    await member.add_roles(role)
+
+    await dm.send(f'{member.name}, welcome to my Discord Server, {random.choice(greetings)}')
+
+    for channel in member.guild.channels:
+        if str(channel.name) == "welcome":
+            await channel.send(f"""Welcome to the server {member.mention}""")
 
 
 @client.event
@@ -28,13 +36,20 @@ async def on_message(message):
     id = client.get_guild(484842929865883648)
     channels = ["bot"]
     greetings=['where have you been',"I've been expecting you",'how can I help you today']
+    bad_words = ["fuck", "Fuck", "dick","Dick"]
 
     if str(message.channel) in channels:
-        if message.content.find("Hello","hello") != -1:
+        if message.content.find("!hello") != -1:
             await message.channel.send(f"Hi there {message.author.name}, {random.choice(greetings)} !") 
         elif message.content == "!users":
             await message.channel.send(f"""This server has {id.member_count} member(s)!""")
         elif message.content == "!members":
-            await message.channel.send(f"""This server has {id.member_count} member(s)!""")    
+            await message.channel.send(f"""This server has {id.member_count} member(s)!""")
+
+    for word in bad_words:
+        if message.content.count(word) > 0:
+            print("A bad word was said")
+            await message.channel.purge(limit=1)
+            await message.channel.send("Words like this are not permited in this server!")
 
 client.run(token)
