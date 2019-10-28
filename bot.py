177851378324bcbfb53eff1c06408631ea583edc
Stefan Ciubotaru <stefanelul2000@users.noreply.github.com>
2019-10-28 17:33:35 +0000

    Reorganisation
    Moved Gif function to another file (gif.py)
    Purge "Working on it..." message after requesting the stock history

--- bot.py
+++ bot.py
@@ -6,34 +6,19 @@ from discord.ext import commands
 from discord.ext.commands import Bot
 import graph
 import analyse_text
-import giphy_client
-from giphy_client.rest import ApiException
-#from pprint import pprint
+import gif
 from dotenv import load_dotenv
 import time
+import asyncio
+import datetime
 
 load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
-giphy_token= os.getenv('GIPHY_TOKEN')
 
 client = discord.Client()
 bot_prefix = "$"
 client = commands.Bot(command_prefix=bot_prefix)
-api_instance = giphy_client.DefaultApi() #Giphy API
 
-def search_gifs(query):
-    try:
-        return api_instance.gifs_search_get(giphy_token, query, limit=5, rating = 'g')
-
-    except ApiException as e:
-        return "Exception when calling DefaultApi->gifs_search_get: %s\n" % e
-
-def gif_response(emotion):
-    gifs = search_gifs(emotion)
-    lst = list(gifs.data)
-    gif = random.choices(lst)
-
-    return gif[0].url
 
 @client.event
 async def on_ready():
@@ -56,7 +41,7 @@ async def on_member_join(member):
     for channel in member.guild.channels:
         if str(channel.name) == "welcome":
             await channel.send(f"""Welcome to the server {member.mention}""")
-            await channel.send(gif_response('welcome'))
+            await channel.send(gif.gif_response('welcome'))
 
 
 @client.event
@@ -72,14 +57,14 @@ async def on_message(message):
     if str(message.channel) in channels:
         if message.content.find("!hello") != -1:
             await message.channel.send(f"Hi there {message.author.name}, {random.choice(greetings)} !") 
-            await message.channel.send(gif_response('hello'))
+            await message.channel.send(gif.gif_response('hello'))
         elif message.content == "!users":
             await message.channel.send(f"""This server has {id.member_count} member(s)!""")
         elif message.content == (f"{client.user} How many Members does the server have?"):
             await message.channel.send(f"""This server has {id.member_count} member(s)!""")
         elif message.content == "!thanks":
             await message.channel.send(f"{random.choice(thanks)} {message.author.name}")
-            await message.channel.send(gif_response('thanks'))         
+            await message.channel.send(gif.gif_response('thanks'))         
     for word in bad_words:
         if message.content.count(word) > 0:
             await message.channel.purge(limit=1)
@@ -88,12 +73,16 @@ async def on_message(message):
 
 @client.command()
 async def ask(ctx,*,arg):
-    myOrganisation , myTimeFrame, myDays = analyse_text.process_text(arg)
-    graph.information_type("closed",time_scale=myTimeFrame,days=myDays,company_name=myOrganisation)
     await ctx.send("Working on it...")
-
+    channel = ctx.message.channel
+    myOrganisation , myTimeFrame, myDays = analyse_text.process_text(arg)
+    print(myOrganisation, myTimeFrame, myDays)
+    graph.information_type("closed",time_scale=myTimeFrame,days=myDays,company_name=myOrganisation)
+    await channel.purge(limit=1,check=None,bulk=True)
     await ctx.send(file=discord.File('stockImage.png'))
-    await ctx.send(f"Here you go {message.author.name}, showing you {myOrganisation} stock.")
+    await ctx.send(f"Here you go {ctx.author.mention}, showing you {myOrganisation} stock.")
+    await asyncio.sleep(1)
+    await ctx.send(gif.gif_response('looks expensive'))     
 
 @client.command(pass_context = True)
 async def clear(ctx, ammount=100):
